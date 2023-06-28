127226f3fa0076a9ba2fc608e0dcd62e6eadea17
Muhammad Fazel <fazelsamar@gmail.com>
2023-06-28 22:12:50 +0330

    Added cron job

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"log"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/the-go-dragons/final-project2/internal/app"
 	"github.com/the-go-dragons/final-project2/pkg/config"
+	"github.com/the-go-dragons/final-project2/pkg/cronjob"
 	"github.com/the-go-dragons/final-project2/pkg/database"
 )
 
@@ -14,5 +15,6 @@ func main() {
 	database.AutoMigrateDB()
 	app := app.NewApp()
 	// seeder.Run()
-	log.Fatalln(app.Start(config.GetEnv("EXPOSE_PORT")))
+	cronjob.NewCronJobRunner()
+	log.Info(app.Start(config.GetEnv("EXPOSE_PORT")))
 }
