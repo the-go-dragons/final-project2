4087e6785ce5147377027cecb70632861c757318
eisa godari <eisa.godari@gmail.com>
2023-07-06 18:34:05 +0330

    Add default type to number model

--- internal/domain/number.go
+++ internal/domain/number.go
@@ -9,17 +9,18 @@ import (
 type NumberTypeEnum byte
 
 const (
-    Sale NumberTypeEnum = 1
-    Rent NumberTypeEnum = 2
+	Sale   NumberTypeEnum = 1
+	Rent   NumberTypeEnum = 2
+	Public NumberTypeEnum = 3
 )
 
 type Number struct {
 	gorm.Model
-	ID          uint      `json:"id"`
-	Phone       string    `json:"phone"`
-	Price       uint32      `json:"price"`
-	IsAvailable bool      `json:"isAvailable" gorm:"default:true"`
-	Type        NumberTypeEnum  `json:"type" gorm:"default:1"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	ID          uint           `json:"id"`
+	Phone       string         `json:"phone"`
+	Price       uint32         `json:"price"`
+	IsAvailable bool           `json:"isAvailable" gorm:"default:true"`
+	Type        NumberTypeEnum `json:"type" gorm:"default:1"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updatedAt"`
 }
